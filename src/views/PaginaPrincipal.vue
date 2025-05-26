6a30dbdb0858ff0990b7c4da622a6d9eb2ee6913
alaquoke <alaquoke@gmail.com>
2025-05-26 15:11:23 -0300

    WiP: "porque usar" section

--- src/views/PaginaPrincipal.vue
+++ src/views/PaginaPrincipal.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Welcome from "../components/Welcome.vue";
+import PorqueUsar from "../components/PorqueUsar.vue";
 </script>
 
 
 
 <template>
   <Welcome />
+  <PorqueUsar />
 </template>
